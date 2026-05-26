34004bc16dcd215d66812fe62a1d4c1c05d02f18
aSalah92 <asalahr7@gmail.com>
2026-05-26 11:05:47 +0300

    done with html tags selection in JS file

---
+++ js/script.js
@@ -0,0 +1,42 @@
+// img-box elements
+let rating = document.getElementById("rating-stars");
+let views = rating.children[0];
+let prepTime = document.getElementsByClassName("details-text")[0];
+let cookTime = document.getElementsByClassName("details-text")[1];
+let servings = document.getElementsByClassName("details-text")[2];
+
+//text-box element
+let difficulty = document.getElementById("difficulty");
+let region = document.getElementById("region");
+
+let mealName = document.getElementById("meal-name");
+let mealDisc = document.getElementById("meal-disc");
+
+let warningMsg = document.getElementById("warning-box");
+
+let ingredientsPage = document.getElementById("ingredients-page");
+// ingredients-page form
+// <div class="ingredient-box d-flex">
+//   <div class="num-box"><p>1</p></div>
+//   <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit.</p>
+// </div>
+
+let instOne = document.getElementById("inst-one");
+let instTwo = document.getElementById("inst-two");
+let instThree = document.getElementById("inst-three");
+let instFour = document.getElementById("inst-four");
+let instFive = document.getElementById("inst-five");
+let instSix = document.getElementById("inst-six");
+
+let calories = document.getElementsByClassName("nutrition-values")[0];
+let protein = document.getElementsByClassName("nutrition-values")[1];
+let carbohydrates = document.getElementsByClassName("nutrition-values")[2];
+let fat = document.getElementsByClassName("nutrition-values")[3];
+let fiber = document.getElementsByClassName("nutrition-values")[4];
+let sodium = document.getElementsByClassName("nutrition-values")[5];
+
+let chefPage = document.getElementById("chef-inner");
+//   <div class="chef-box d-flex gap-2 my-4">
+//     <p class="m-0">✓</p>
+//     <p class="m-0">Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident, doloribus.</p>
+//   </div>
